dfc952da9ec0fd7f6118e73718df5c0c4da8dfa3
Samuel <samuelmodestoes@gmail.com>
2022-05-25 23:28:15 -0300

    finalized app samCard

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,19 +13,20 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.indigo,
         body: SafeArea(
           child: Column(
-            children: [
-              const CircleAvatar(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              CircleAvatar(
                 radius: 50.0,
                 backgroundImage: AssetImage('images/fotoSamuel.jpg'),
               ),
-              const Text(
+              Text(
                 'Samuel Modesto',
                 style: TextStyle(
                     fontFamily: 'Pacifico',
                     fontSize: 40.0,
                     color: Colors.white),
               ),
-              const Text(
+              Text(
                 'SOFTWARE ENGINEER',
                 style: TextStyle(
                   fontFamily: 'Source Sans Pro',
@@ -33,50 +35,45 @@ class MyApp extends StatelessWidget {
                   letterSpacing: 2.5,
                 ),
               ),
-              Container(
-                color: Colors.white,
-                margin: const EdgeInsets.symmetric(
-                    vertical: 10.0, horizontal: 25.0),
-                padding: const EdgeInsets.all(10.0),
-                child: Row(
-                  children: const [
-                    Icon(
-                      Icons.phone,
-                      color: Colors.indigo,
-                    ),
-                    SizedBox(width: 10.0),
-                    Text(
-                      '+55 11 5555-5555',
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 20.0,
-                          fontFamily: 'Source Sans Pro'),
-                    ),
-                  ],
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.white,
                 ),
               ),
-              Container(
-                padding: const EdgeInsets.all(10.0),
-                margin: const EdgeInsets.symmetric(
-                    vertical: 10.0, horizontal: 25.0),
+              Card(
                 color: Colors.white,
-                child: Row(
-                  children: const [
-                    Icon(
-                      Icons.email,
-                      color: Colors.indigo,
-                    ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Text(
-                      'samuelmodestoes@gmail.com',
-                      style: TextStyle(
-                          fontSize: 20.0,
-                          fontFamily: 'Source Sans Pro',
-                          color: Colors.black),
-                    )
-                  ],
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.indigo,
+                  ),
+                  title: Text(
+                    '+55 11 5555-5555',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20.0,
+                        fontFamily: 'Source Sans Pro'),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                color: Colors.white,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.indigo,
+                  ),
+                  title: Text(
+                    'samuelmodestoes@gmail.com',
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        fontFamily: 'Source Sans Pro',
+                        color: Colors.black),
+                  ),
                 ),
               )
             ],
@@ -86,3 +83,11 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+// Row(
+// children: const [
+//
+// SizedBox(width: 10.0),
+//
+// ],
+// ),
